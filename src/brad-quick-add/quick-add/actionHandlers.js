0ee3aef0f45015bd0298724966fb920c0bc6b162
Brad Tilton <brad.tilton@servicenow.com>
2020-10-09 16:20:17 -0500

    added type on LCHH

--- src/brad-quick-add/quick-add/actionHandlers.js
+++ src/brad-quick-add/quick-add/actionHandlers.js
@@ -10,10 +10,20 @@ export default {
             const artifactTitle = value;
             updateState({artifactTitle});
         },
+        'NOW_DROPDOWN#SELECTED_ITEMS_SET': (coeffects) => {
+            const {action, state, updateState} = coeffects;
+            const {value} = action.payload;
+            console.log(JSON.stringify(value[0]));
+            const artifactType = value[0];
+            updateState({artifactType});
+        },
         'NOW_BUTTON#CLICKED': (coeffects) => {
             const {state, dispatch} = coeffects;
             dispatch('CREATE_ARTIFACT', {
-                data: {'short_description' : state.artifactTitle}
+                data: {
+                    'short_description' : state.artifactTitle,
+                    'type': state.artifactType
+                }
             });
         },
         'CREATE_ARTIFACT': createHttpEffect(ARTIFACT_ENDPOINT, {
